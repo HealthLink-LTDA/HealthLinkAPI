5bec00605de5e714c7956e37829bc1c08c29525d
Riume <riumecarlos@hotmail.com>
2024-12-14 19:05:17 -0300

    docs(project): adicionando swagger para documentação e implementando nas rotas

--- src/paciente/paciente.controller.ts
+++ src/paciente/paciente.controller.ts
@@ -11,8 +11,16 @@ import {
 } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { PacienteService } from './paciente.service';
+import {
+  ApiTags,
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 @Controller('paciente')
+@ApiTags('Paciente')
+@ApiBearerAuth()
 export class PacienteController {
   private readonly logger = new Logger(PacienteController.name);
 
@@ -20,6 +28,8 @@ export class PacienteController {
 
   @UseGuards(JwtAuthGuard)
   @Get()
+  @ApiOperation({ summary: 'Get all pacientes' })
+  @ApiResponse({ status: 200, description: 'List of pacientes' })
   findAll() {
     this.logger.log('Fetching all pacientes');
     return this.pacienteService.findAll();
@@ -27,6 +37,8 @@ export class PacienteController {
 
   @UseGuards(JwtAuthGuard)
   @Post()
+  @ApiOperation({ summary: 'Create a new paciente' })
+  @ApiResponse({ status: 201, description: 'Paciente created successfully' })
   create(
     @Body('nome') nome: string,
     @Body('cpf') cpf: string,
@@ -46,6 +58,8 @@ export class PacienteController {
 
   @UseGuards(JwtAuthGuard)
   @Get(':id')
+  @ApiOperation({ summary: 'Get a paciente by ID' })
+  @ApiResponse({ status: 200, description: 'Paciente details' })
   getById(@Param('id') id: string) {
     this.logger.log(`Fetching paciente by ID: ${id}`);
     return this.pacienteService.findOneById(id);
@@ -53,6 +67,8 @@ export class PacienteController {
 
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a paciente by ID' })
+  @ApiResponse({ status: 200, description: 'Paciente deleted successfully' })
   remove(@Param('id') id: string) {
     this.logger.log(`Deleting paciente with ID: ${id}`);
     return this.pacienteService.remove(id);
@@ -60,6 +76,8 @@ export class PacienteController {
 
   @UseGuards(JwtAuthGuard)
   @Put(':id')
+  @ApiOperation({ summary: 'Update a paciente by ID' })
+  @ApiResponse({ status: 200, description: 'Paciente updated successfully' })
   update(
     @Param('id') id: string,
     @Body('nome') nome: string,
