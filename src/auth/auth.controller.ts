692bc875f657bc71864c31bea0a3bc2b14fe4f80
Riume <riumecarlos@hotmail.com>
2024-12-14 15:37:58 -0300

    fix(project): arrumando CORS error

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  private readonly logger = new Logger(AuthController.name);
+
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   async login(@Body() loginDto: { email: string; senha: string }) {
+    this.logger.log(`Login attempt with email: ${loginDto.email}`);
     return this.authService.login(loginDto);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowedOrigins = configService
+        .get<string>('CORS_ALLOWED_ORIGINS')
+        .split(',');
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`Origin ${origin} not allowed by CORS policy`));
+      }
+    },
+    allowedHeaders: configService.get<string>('CORS_ALLOWED_HEADERS'),
+    credentials: configService.get<boolean>('CORS_CREDENTIALS'),
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
